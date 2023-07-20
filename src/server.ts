9815e508b499c07d9f77d1446ee134bcdc310dc4
Lart <tlartigau.pro@gmail.com>
2023-07-20 14:50:47 +0200

    fix: import for jobs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { tokenRouter, premiumRouter, saveRouter } from "./routes"
+import resetCounters from "./jobs/resetCounters"
 import bodyParser from "body-parser"
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
 import helmet from "helmet"
 import mongoose from "mongoose"
-import resetCounters from "jobs/resetCounters"
 
 dotenv.config()
 
